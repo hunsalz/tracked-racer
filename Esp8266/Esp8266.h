d0da5003b56ea005c6638bff148bb3f2d06ea7f2
hunsalz <info@hunsalz.de>
2018-01-05 00:32:44 +0100

    changed all log4arduino references to log4Esp

--- Esp8266/Esp8266.h
+++ Esp8266/Esp8266.h
@@ -3,28 +3,17 @@
 #include <Arduino.h> // https://github.com/esp8266/Arduino/tree/master/cores/esp8266
 #include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson
 
-#include <Logger.h> // https://github.com/hunsalz/log4arduino
-
-#include <EspService.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/EspService.h
-#include <FSService.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/FSService.h
-#include <network/MDNSService.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/network/MDNSService.h
-#include <network/MQTTService.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/network/MQTTService.h
-#include <network/WiFiAPService.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/network/WiFiAPService.h
-#include <network/WiFiService.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/network/WiFiService.h
-#include <WebService.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/WebService.h
-#include <WebSocketListener.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/WebSocketListener.h
-
-#include <actuator/MotorDriver.h> // https://github.com/hunsalz/esp8266utils/blob/master/src/actuator/MotorDriver.h
+#include <Log4Esp.h> // https://github.com/hunsalz/log4Esp
+#include <Esp8266Utils.h> // https://github.com/hunsalz/esp8266utils
 
 using esp8266util::MotorDriver;
-using log4arduino::LOG;
+using log4Esp::LOG;
 using esp8266util::FILESYSTEM;
 using esp8266util::MDNS_SERVICE;
 using esp8266util::SERVER;
 using esp8266util::SYSTEM;
 using esp8266util::WIFI_CLIENT;
 using esp8266util::WIFI_STATION;
-
 
 class Esp8266 {
 
