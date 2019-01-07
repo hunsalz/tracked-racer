da63c8be3525ef7c57c6dbc56e97847d190641fe
hunsalz <info@hunsalz.de>
2019-01-07 22:55:41 +0100

    reflected esp8266utils changes; WS event handling still in progress

--- Esp8266/Esp8266.ino
+++ Esp8266/Esp8266.ino
@@ -1,134 +1,178 @@
-#include <Esp8266Utils.h>  // https://github.com/hunsalz/esp8266utils
-#include <Log4Esp.h>       // https://github.com/hunsalz/log4Esp
+#include <ESP8266mDNS.h>        // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266mDNS/src/ESP8266mDNS.h
+#include <ESP8266WiFiMulti.h>   // https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi/src/ESP8266WiFiMulti.h
+#include <StreamString.h>       // https://github.com/esp8266/Arduino/blob/master/cores/esp8266/StreamString.h
+
+#include <Esp8266Utils.h>       // https://github.com/hunsalz/esp8266utils
 
 #include "config.h"
-
-// web server settings
-const static int PORT = 80;
 
 esp8266utils::MotorDriver _motorA;
 esp8266utils::MotorDriver _motorB;
 
 esp8266utils::WebSocketListener _wsl;
 
-void setup() {
-  // logger setup
-  LOG.addLevelToAll(Appender::VERBOSE);
-  LOG.addFormatterToAll(
-      [](Print &output, Appender::Level level, const char *msg, va_list *args) {
-        // output log level
-        output.print(Appender::toString(level, true));
-        output.print(Appender::DEFAULT_SEPARATOR);
-        // output uptime of this program in milliseconds
-        output.print(millis());
-        output.print(Appender::DEFAULT_SEPARATOR);
-        // output free heap space
-        output.print(ESP.getFreeHeap());
-        output.print(Appender::DEFAULT_SEPARATOR);
-        // determine buffer length for formatted data
-        size_t length = vsnprintf(NULL, 0, msg, *args) + 1;
-        char buffer[length];
-        // output formatted data
-        vsnprintf(buffer, length, msg, *args);
-        output.print(buffer);
-      });
+unsigned long nextLoopInterval = 0;
 
-  // serial setup
-  Serial.begin(115200);
-  Serial.setDebugOutput(false);
-  while (!Serial && !Serial.available()) {
-  };
-  Serial.println();
-  LOG.verbose(F("Serial baud rate is [%d]"), Serial.baudRate());
+void setup() {
+  
+  // init Serial with desired baud rate
+  esp8266utils::Logging::init(115200);
+  VERBOSE_FP(F("Serial baud rate is [%lu]"), Serial.baudRate());
 
   // setup hardware components
-  _motorA.begin(MOTOR_A_PWM, MOTOR_A_DIR),
-      _motorB.begin(MOTOR_B_PWM, MOTOR_B_DIR),
-      esp8266utils::MotorDriver::setPWMRange(PWM_RANGE);
+  _motorA.begin(MOTOR_A_PWM, MOTOR_A_DIR);
+  _motorB.begin(MOTOR_B_PWM, MOTOR_B_DIR);
+  //esp8266utils::setPWMRange(PWM_RANGE);
 
-  // WiFi setup
-  WIFI_STA_CFG.addAP(WIFI_SSID_1, WIFI_PSK_1);
-  WIFI_STA_CFG.addAP(WIFI_SSID_2, WIFI_PSK_2);
-  WIFI_STA_CFG.begin();
+  // WiFi AP setup
+  esp8266utils::setupWiFiAp(WIFI_AP_SSID, WIFI_AP_PSK);
 
-  WIFI_AP_CFG.begin(WIFI_AP_SSID, WIFI_AP_PSK);
+//  // WiFi station setup
+//  ESP8266WiFiMulti wifiMulti;
+//  wifiMulti.addAP(WIFI_SSID_1, WIFI_PSK_1);
+//  wifiMulti.addAP(WIFI_SSID_2, WIFI_PSK_2);
+//  esp8266utils::setupWiFiSta(wifiMulti);
 
   // MDNS setup
-  MDNS_CFG.begin("esp8266");
-  MDNS.addService("http", "tcp", PORT);
-
-  // increase loop interval
-  SYS_CFG.setLoopInterval(20000);
+  const char* hostname = "esp8266";
+  if (MDNS.begin(hostname)) {
+    // add service to MDNS-SD
+    MDNS.addService("http", "tcp", 80);
+    VERBOSE_FP(F("MDNS enabled to http://%s.local"), hostname);
+  } else {
+     ERROR_FP(F("MDNS failed for http://%s.local"), hostname);
+  }
 
   // file system setup to enable static web server content
-  FILESYSTEM.begin();
+  esp8266utils::FileSystem fs; 
+  fs.begin();
 
-  // web server setup
-  SERVER.begin();
+  // general web server setup
+  esp8266utils::WebService webService(80);
+  webService.begin();
   // rewrite root context
-  SERVER.getWebServer().rewrite("/", "/index.build.html");
+  webService.getWebServer().rewrite("/", "/index.html");
   // handle static web resources
-  SERVER.getWebServer().serveStatic("/", SPIFFS, "/www/", "max-age:15");
-  // cache-control 15 seconds
+  webService.getWebServer().serveStatic("/", SPIFFS, "/www/", "max-age:15");
+
   // add dynamic http resources
-  SERVER.on("/fs", HTTP_GET, [](AsyncWebServerRequest *request) {
-    SERVER.send(request, esp8266utils::APP_JSON, FILESYSTEM.getStorageDetails());
+  webService.on("/fs", HTTP_GET, [&fs](AsyncWebServerRequest* request) {
+
+    AsyncResponseStream *response = request->beginResponseStream("application/json");  
+    StreamString* payload = new StreamString();
+    size_t size = fs.serializeInfo(*payload);
+    response->print(*payload); 
+    request->send(response);
+    delete payload;
   });
-  SERVER.on("/files", HTTP_GET, [](AsyncWebServerRequest *request) {
-    SERVER.send(request, esp8266utils::APP_JSON, FILESYSTEM.getFileListing());
+  webService.on("/files", HTTP_GET, [&fs](AsyncWebServerRequest* request) {
+
+    AsyncResponseStream *response = request->beginResponseStream("application/json");  
+    StreamString* payload = new StreamString();
+    size_t size = fs.serializeListing(*payload);
+    response->print(*payload); 
+    request->send(response);
+    delete payload;
   });
-  SERVER.on("/sta", HTTP_GET, [](AsyncWebServerRequest *request) {
-    SERVER.send(request, esp8266utils::APP_JSON, WIFI_STA_CFG.getDetails());
+  webService.on("/ap", HTTP_GET, [](AsyncWebServerRequest* request) {
+
+    AsyncResponseStream *response = request->beginResponseStream("application/json");  
+    StreamString* payload = new StreamString();
+    size_t size = esp8266utils::serializeWiFiAp(*payload);
+    response->print(*payload); 
+    request->send(response);
+    delete payload;
   });
-  SERVER.on("/ap", HTTP_GET, [](AsyncWebServerRequest *request) {
-    SERVER.send(request, esp8266utils::APP_JSON, WIFI_AP_CFG.getDetails());
+//  webService.on("/sta", HTTP_GET, [](AsyncWebServerRequest* request) {
+//
+//    AsyncResponseStream *response = request->beginResponseStream("application/json");  
+//    StreamString* payload = new StreamString();
+//    size_t size = esp8266utils::serializeWiFiSta(*payload);
+//    response->print(*payload); 
+//    request->send(response);
+//    delete payload;
+//  });
+  webService.on("/esp", HTTP_GET, [](AsyncWebServerRequest* request) {
+    
+    AsyncResponseStream *response = request->beginResponseStream("application/json");  
+    StreamString* payload = new StreamString();
+    size_t size = esp8266utils::serializeESP(*payload);
+    response->print(*payload); 
+    request->send(response);
+    VERBOSE(*payload);
+    delete payload;
   });
-  SERVER.on("/esp", HTTP_GET, [](AsyncWebServerRequest *request) {
-    SERVER.send(request, esp8266utils::APP_JSON, SYS_CFG.getDetails());
+  webService.on("/motor_a", HTTP_GET, [](AsyncWebServerRequest* request) {
+    
+    AsyncResponseStream *response = request->beginResponseStream("application/json");  
+    StreamString* payload = new StreamString();
+    size_t size = _motorA.serialize(*payload);
+    response->print(*payload); 
+    request->send(response);
+    VERBOSE(*payload);
+    delete payload;
   });
-  SERVER.on("/motor_a", HTTP_GET, [](AsyncWebServerRequest *request) {
-    SERVER.send(request, esp8266utils::APP_JSON, _motorA.getDetails());
+  webService.on("/motor_b", HTTP_GET, [](AsyncWebServerRequest* request) {
+    
+    AsyncResponseStream *response = request->beginResponseStream("application/json");  
+    StreamString* payload = new StreamString();
+    size_t size = _motorB.serialize(*payload);
+    response->print(*payload);
+    VERBOSE(*payload);
+    request->send(response);
+    VERBOSE(*payload);
+    delete payload;
   });
-  SERVER.on("/motor_b", HTTP_GET, [](AsyncWebServerRequest *request) {
-    SERVER.send(request, esp8266utils::APP_JSON, _motorB.getDetails());
-  });
+  
   // add web socket support
   _wsl.onTextMessage([](AsyncWebSocket *ws, AsyncWebSocketClient *client, AwsEventType type, AwsFrameInfo *info, uint8_t *data, size_t len) {
 
-    DynamicJsonBuffer buffer;
-    JsonObject &json = buffer.parse((char *)data);
+    // extract request payload
+    char payload[len + 1];
+    snprintf(payload, len + 1, (char*)data);
+    VERBOSE_FP(F("Payload of request is: %s"), payload);
 
-    if (json.success() && json["motorA"].success() && json["motorB"].success() && json["mode"].success()) {
-      int speedA = json["motorA"];
-      int speedB = json["motorB"];
+    client->text("DONE");
+    return;
 
-      const char *mode = json["mode"];
-
-      LOG.verbose(F("Set motor A = %d and motor B = %d, PWM range = %d"), speedA, speedB, _motorA.getPWMRange());
-
-      // decide which mode to use
-      if (strcmp("absolute", mode) == 0) {
-        _motorA.setSpeed(speedA);
-        _motorB.setSpeed(speedB);
-      } else {
-        _motorA.applySpeed(speedA);
-        _motorB.applySpeed(speedB);
-      }
-
-      // create JSON reply message
-      DynamicJsonBuffer buffer;
-      JsonObject &message = buffer.createObject();
-      message["clientId"] = client->id();
-      message["motorA"] = _motorA.getSpeed();
-      message["motorB"] = _motorB.getSpeed();
-
-      // send reply message
-      uint16_t length = message.measureLength() + 1;
-      char payload[length];
-      message.printTo(payload, length);
-      client->text(payload);
-    } else {
+    // try to parse payload as JSON
+    DynamicJsonDocument doc;
+    DeserializationError err = deserializeJson(doc, payload);
+    if (err) {
+      VERBOSE_F("Reading request failed: %s", err.c_str());
       client->text(F("Received an unexpected message."));
+    } else {
+//      JsonObject request = doc.as<JsonObject>();
+//      int speedA = request["motorA"];
+//      int speedB = request["motorB"];
+//      char* mode = request["mode"];
+//      // decide which process mode to use
+//      if (strcmp("absolute", mode) == 0) {
+//        _motorA.setSpeed(speedA);
+//        _motorB.setSpeed(speedB);
+//      } else {
+//        _motorA.applySpeed(speedA);
+//        _motorB.applySpeed(speedB);
+//      }
+
+      
+      // create JSON response
+//      DynamicJsonDocument doc2;
+//      JsonObject response = doc2.to<JsonObject>();
+//      response["clientId"] = client->id();
+//      response["motorA"] = _motorA.getSpeed();
+//      response["motorB"] = _motorB.getSpeed();
+
+//      StreamString* payload = new StreamString();
+//      serializeJson(response, payload);
+//      size_t size = measureJson(response);
+      //client->text(*payload);
+
+//      // send reply message
+//      uint16_t length = message.measureLength() + 1;
+//      char payload[length];
+//      message.printTo(payload, length);
+//      client->text(payload);
     }
   });
   // add web socket
@@ -136,20 +180,21 @@ void setup() {
   webSocket->onEvent([](AsyncWebSocket *ws, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) { 
     _wsl.onEvent(ws, client, type, arg, data, len);
   });
-  SERVER.getWebServer().addHandler(webSocket);
+  webService.getWebServer().addHandler(webSocket);
 
-  LOG.verbose(F("========================="));
-  LOG.verbose(F("Setup finished. Have fun."));
-  LOG.verbose(F("========================="));
+  VERBOSE_FP(F("========================="));
+  VERBOSE_FP(F("Setup finished. Have fun."));
+  VERBOSE_FP(F("========================="));
 }
 
 void loop() {
-  
-  ESP.wdtDisable();
-  if (SYS_CFG.nextLoopInterval()) {
+
+  if (millis() > nextLoopInterval) {  
+    nextLoopInterval = millis() + 5000;
+    
     MDNS.update();
   }
-  ESP.wdtEnable(30000);
+
   // reserve time for core processes
   delay(500);
 }
